d7ff671af473faf7fdc01783eadd0aff7fe0dfcb
LongGuann <3539557218@qq.com>
2026-01-28 15:01:44 +0800

    增加了队列的链表实现

--- 数据结构/code/Day04/04单向链表实现队列.c
+++ 数据结构/code/Day04/04单向链表实现队列.c
@@ -48,7 +48,7 @@ void push(LinkedListQueue *q, int data)
 
     if (isEmpty(q))
     {
-        // 若队列为空队列第一个入队元素既是对首也是队尾
+        // 若队列为空队列第一个入队元素既是队首也是队尾
         q->front = n;
     }
     else
@@ -58,6 +58,42 @@ void push(LinkedListQueue *q, int data)
 
     q->rear = n; // 让节点变为新的队尾
     q->size++;
+}
+
+// 访问队首元素
+int peek(LinkedListQueue *q)
+{
+    if (isEmpty(q))
+    {
+        printf("队列为空!\n");
+        return -1;
+    }
+    return q->front->data;
+}
+
+int pop(LinkedListQueue *q)
+{
+    if (isEmpty(q))
+    {
+        return -1;
+    }
+    // 获取队首元素的值,用于返回到函数外部
+    int res = peek(q);
+
+    Node *tmp = q->front;
+    q->front = q->front->next;
+    free(tmp);
+    q->size--;
+
+    return res;
+}
+
+void delLinkedListQueue(LinkedListQueue *q) {
+    while (!isEmpty(q))
+    {
+        pop(q);
+    }
+    free(q);
 }
 
 int main(int argc, char const *argv[])
@@ -72,6 +108,14 @@ int main(int argc, char const *argv[])
     push(que, 8);
     push(que, 9);
     push(que, 10);
+
+    // printf("%d \n", pop(que));
+    // printf("%d \n", pop(que));
+    // printf("%d \n", pop(que));
+    // printf("%d \n", pop(que));
+    // printf("%d \n", pop(que));
+
+    delLinkedListQueue(que);
 
     Node *p = que->front;
     while (p != NULL)
